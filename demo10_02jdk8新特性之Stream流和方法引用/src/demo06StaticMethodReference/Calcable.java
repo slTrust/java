6b31481f05c4b950cb56654df3150b882268bb39
huangjx <trustfor@sina.cn>
2019-03-26 18:02:21 +0800

    java基础部分代码
    java基础部分代码

---
+++ demo10_02jdk8新特性之Stream流和方法引用/src/demo06StaticMethodReference/Calcable.java
@@ -0,0 +1,7 @@
+package demo06StaticMethodReference;
+
+@FunctionalInterface
+public interface Calcable {
+    //定义一个抽象方法,传递一个整数,对整数进行绝对值计算并返回
+    int calsAbs(int number);
+}
---
+++ demo10_02jdk8新特性之Stream流和方法引用/src/demo07SuperMethodReference/Human.java
@@ -0,0 +1,10 @@
+package demo07SuperMethodReference;
+/*
+    定义父类
+ */
+public class Human {
+    //定义一个sayHello的方法
+    public void sayHello(){
+        System.out.println("Hello 我是Human!");
+    }
+}
